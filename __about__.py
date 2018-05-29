14b852f1e9ab0ece6ca2fe5fe75f97239f3de573
ewerlopes <ewerlopes@gmail.com>
2018-05-29 12:26:48 +0200

    refactoring

--- __about__.py
+++ __about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-   variational_gmm.__about__
+   variational_inference.__about__
    ~~~~~~~~~~~~~~~~~~~
 """
 __version__ = '0.0.1'
@@ -8,4 +8,4 @@ __license__ = 'BSD'
 __author__ = 'Ewerton Lopes'
 __author_email__ = 'ewerton.lopes@polimi.it'
 __url__ = 'ewerlopes.github.io'
-__description__ = 'The implementation of variational inference for GMM'
+__description__ = 'An implementation of variational inference for some models'
